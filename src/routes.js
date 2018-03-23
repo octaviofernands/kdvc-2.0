5ae27ed66a2f92c4ca4a860e5321666ae1d1f73c
leticiaalves <leticia_borges.alves@hotmail.com>
2018-03-23 14:00:50 -0300

    Validando Cadastro de Usuários

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const ROUTER = Router()
 ROUTER.post('/user/signup', (req, res) => {
     simpleRegister(req).then((data) => {
         res.json(data)
+    }).catch((error) => {
+        res.status(400).json(error)
     })
 })
 
